2e56126a44e8d36d67c930a8e65e2d81ee064d64
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-05-16 08:13:46 +0530

    20240515

--- vb/console/basic/sum.vb
+++ vb/console/basic/sum.vb
@@ -1,4 +1,11 @@
-a = console.readline()
-b = console.readline()
-c = a + b
-console.write(c)
+Module Sum
+	Sub Main()
+		Dim a, b, c as Integer
+		Console.Write("Enter a number: ")
+		a = Console.Readline()
+		Console.Write("Enter another number: ")
+		b = Console.Readline()
+		c = a + b
+		Console.Write("The sum is {0}", c)
+	End Sub
+End Module
