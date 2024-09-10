3ca1db490be24a212eed99ec8c776e637e03f295
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-09-10 12:01:33 +0530

    20240910

--- dsa/cpp/linkedlist/linkedlist-operations.cpp
+++ dsa/cpp/linkedlist/linkedlist-operations.cpp
@@ -18,6 +18,11 @@ namespace linkedlist
             this->data = data;
             next = NULL;
         }
+        Node(int data, Node *next)
+        {
+            this->data = data;
+            this->next = next;
+        }
 
         void setData(int data)
         {
@@ -29,12 +34,12 @@ namespace linkedlist
             return data;
         }
 
-        void setNext()
+        void setNext(Node *next)
         {
-
+            this->next = next;
         }
 
-        Node* getNext()
+        Node *getNext()
         {
             return next;
         }
@@ -46,6 +51,9 @@ namespace linkedlist
 
     public:
         void display();
+        void append(int data);
+        void insert(int data);
+        void insert(int index, int data);
     };
 
     void LinkedList::display()
@@ -56,12 +64,63 @@ namespace linkedlist
             return;
         }
 
-        head = new Node();
+        Node *cursor = head;
 
-        while (head->getNext() != NULL)
+        cout << endl
+             << "The content of the list is as follows: " << endl;
+        drawline(10);
+        while (1)
         {
-            cout << head->getData() << endl;
+            cout << cursor->getData() << endl;
+            if (cursor->getNext() == NULL)
+                break;
+            cursor = cursor->getNext();
         }
+        drawline(10);
+        cout << endl;
+    }
+
+    void LinkedList::append(int data)
+    {
+        if (head == NULL)
+        {
+            head = new Node(data);
+            return;
+        }
+
+        Node *cursor = head;
+
+        while (cursor->getNext() != NULL)
+            cursor = cursor->getNext();
+
+        cursor->setNext(new Node(data));
+    }
+
+    void LinkedList::insert(int data)
+    {
+        if (head == NULL)
+        {
+            head = new Node(data);
+            return;
+        }
+        head = new Node(data, head);
+    }
+
+    void LinkedList::insert(int index, int data)
+    {
+        if (head == NULL)
+        {
+            head = new Node(data);
+            return;
+        }
+
+        Node *cursor = head;
+        for (int i = 1; i < index; i++)
+        {
+            cursor = cursor->getNext();
+        }
+
+        cursor->setNext(new Node(data, cursor->getNext()));
     }
 }
 
@@ -84,12 +143,15 @@ void messagebox(string message)
 
 int main(int argc, char const *argv[])
 {
-    int response;
+    int response, data, index;
     linkedlist::LinkedList list;
     while (1)
     {
         cout << "MAIN MENU" << endl;
         cout << "1. Display (Traversal)" << endl;
+        cout << "2. Append" << endl;
+        cout << "3. Insert (at the beginning)" << endl;
+        cout << "4. Insert at an index" << endl;
         cout << "Enter choice [0 to exit]: ";
         cin >> response;
         switch (response)
@@ -101,7 +163,31 @@ int main(int argc, char const *argv[])
         case 1:
             list.display();
             break;
+        case 2:
+            cout << endl
+                 << "Enter a value: ";
+            cin >> data;
+            list.append(data);
+            messagebox("Value inserted!");
+            break;
+        case 3:
+            cout << endl
+                 << "Enter a value: ";
+            cin >> data;
+            list.insert(data);
+            messagebox("Value inserted!");
+            break;
+        case 4:
+            cout << endl
+                 << "Enter a value: ";
+            cin >> data;
+            cout << "Enter index: ";
+            cin >> index;
+            list.insert(index, data);
+            messagebox("Value inserted!");
+            break;
         default:
+            messagebox("Invalid option!");
             break;
         }
     }
